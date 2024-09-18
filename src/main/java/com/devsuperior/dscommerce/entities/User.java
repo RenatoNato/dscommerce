b754fa53f7b05a27bd9d9018ca26f83539020e4f
Renato Cézar <renatocezarsilva@hotmail.com>
2024-09-18 19:34:35 -0300

    Associação muitos pra muitos e criação da tabela para associação TB_PRODUCT_CATEGORY

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -15,6 +15,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String phone;
     private String password;
