bb4788700abd3a4425ed07fb43243c5564ad1516
Renato Cézar <renatocezarsilva@hotmail.com>
2024-09-22 23:26:27 -0300

    Adicionando o primeiro controller ao projeto

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public String teste() {
+        return "Teste Get Rest!";
+    }
+
+}
