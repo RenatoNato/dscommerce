cf54572b0f96e5f00fa87eb6a31ea452a3352c37
Renato Cézar <renatocezarsilva@hotmail.com>
2024-09-22 23:44:45 -0300

    Criando as camadas Repository e organizando o back-end

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,16 +1,26 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public String teste() {
-        return "Teste Get Rest!";
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 
 }
